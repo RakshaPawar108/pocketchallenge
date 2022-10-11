c0766d40f721db35b767214833f301776438b6fa
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-10-11 12:51:43 +0530

    chore: configure tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1 className="text-3xl text-blue-900 font-bold">Hello World</h1>
     </div>
   );
 }
