e3fc8e1faca782664dd963687a6bbb0d36820d76
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-10-16 12:50:19 +0530

    style: add button styling default

--- src/components/Buttons/ControlButtons/BackButton.jsx
+++ src/components/Buttons/ControlButtons/BackButton.jsx
@@ -1,3 +1,3 @@
 export const BackButton = () => {
-  return <button>Back</button>;
+  return <button className="bg-white text-slate-400 uppercase py-2 px-4 rounded-xl font-semibold cursor-pointer border-2 border-slate-300 hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out">Back</button>;
 };
--- src/components/Buttons/ControlButtons/NextButton.jsx
+++ src/components/Buttons/ControlButtons/NextButton.jsx
@@ -1,3 +1,3 @@
 export const NextButton = () => {
-  return <button>Next</button>;
+  return <button className="bg-green-500 text-white uppercase py-2 px-4 rounded-xl font-semibold cursor-pointer hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out">Next</button>;
 };
