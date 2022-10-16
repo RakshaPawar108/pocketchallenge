3dacce16fd7198d0346b65f68bd31cde0866c8b3
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-10-16 20:30:42 +0530

    feat: display details of stepper acc to data

--- src/components/Stepper/Stepper.jsx
+++ src/components/Stepper/Stepper.jsx
@@ -1,16 +1,112 @@
-export const Stepper = () => {
-  const displaySteps = (
-    <div className="flex w-full items-center">
-      <div className="relative flex flex-col items-center text-teal-600">
-        <div className="rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex items-center justify-center py-3"></div>
-        <div className="absolute top-0 text-center mt-16 w-32 text-xs font-medium uppercase">
-          Description
+import { useEffect, useRef, useState } from "react";
+import { FaCheck } from "react-icons/fa";
+
+export const Stepper = ({ steps, currentStep }) => {
+  const [newStep, setNewStep] = useState([]);
+  const stepsRef = useRef();
+
+  const updateStep = (stepNumber, steps) => {
+    const newSteps = [...steps];
+    let count = 0;
+
+    while (count < newSteps.length) {
+      // current step
+      if (count === stepNumber) {
+        newSteps[count] = {
+          ...newSteps[count],
+          highlighted: true,
+          selected: true,
+          completed: true,
+        };
+        count++;
+      }
+      // Step completed
+      else if (count < stepNumber) {
+        newSteps[count] = {
+          ...newSteps[count],
+          highlighted: false,
+          selected: true,
+          completed: true,
+        };
+        count++;
+      }
+      // Step Pending
+      else {
+        newSteps[count] = {
+          ...newSteps[count],
+          highlighted: false,
+          selected: false,
+          completed: false,
+        };
+        count++;
+      }
+    }
+
+    return newSteps;
+  };
+
+  useEffect(() => {
+    const stepState = steps.map((step, index) =>
+      Object.assign(
+        {},
+        {
+          description: step,
+          completed: false,
+          highlighted: index === 0 ? true : false,
+          selected: index === 0 ? true : false,
+        }
+      )
+    );
+
+    stepsRef.current = stepState;
+    const current = updateStep(currentStep - 1, stepsRef.current);
+    setNewStep(current);
+  }, [steps, currentStep]);
+
+  const displaySteps = newStep.map((step, index) => {
+    return (
+      <div
+        key={index}
+        className={
+          index !== newStep.length - 1
+            ? "flex w-full items-center"
+            : "flex items-center"
+        }
+      >
+        <div className="relative flex flex-col items-center text-teal-600">
+          <div
+            className={`rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex items-center justify-center py-3 ${
+              step.selected
+                ? "bg-green-600 text-white font-bold border border-green-600"
+                : ""
+            }`}
+          >
+            {step.completed ? (
+              <span className="text-white font-bold text-xl">
+                <FaCheck />
+              </span>
+            ) : (
+              index + 1
+            )}
+          </div>
+          <div
+            className={`absolute top-0 text-center mt-16 w-32 text-xs font-medium uppercase ${
+              step.highlighted ? "text-gray-500" : "text-gray-400"
+            }`}
+          >
+            {step.description}
+          </div>
         </div>
+        {/* Line */}
+        <div
+          className={`flex-auto border-t-2 transition duration-500 ease-in-out ${
+            step.completed ? "border-green-600" : "border-gray-300"
+          }`}
+        ></div>
       </div>
-      {/* Line */}
-      <div className="flex-auto border-t-2 transition duration-500 ease-in-out"></div>
-    </div>
-  );
+    );
+  });
+
   return (
     <div className="mx-4 p-4 flex justify-between items-center">
       {displaySteps}
