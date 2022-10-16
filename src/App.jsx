3d24af286ec2ae46525a46b07a396f91c3bbe477
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-10-16 19:16:00 +0530

    feat: show pages acc to steps using switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import { Stepper, StepperControls } from "./components";
+import { AccountSetup, Complete, PersonalDetails } from "./pages";
+import { steps } from "./utils";
 
 function App() {
+  const [currentStep, setCurrentStep] = useState(1);
+  const displayStep = (step) => {
+    switch (step) {
+      case 1:
+        return <AccountSetup />;
+      case 2:
+        return <PersonalDetails />;
+      case 3:
+        return <Complete />;
+      default:
+    }
+  };
   return (
     <div className="App md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
       <div className="container horizontal mt-5">
-        <Stepper />
+        <Stepper steps={steps} currentStep={currentStep} />
       </div>
       <StepperControls />
     </div>
