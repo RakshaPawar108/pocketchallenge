573d2de5b1018b2f188146c94dab32add22645ef
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-10-16 11:59:01 +0530

    style: app component styling for container

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { Stepper, StepperControls } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container vertical">
+    <div className="App md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
+      <div className="container horizontal mt-5">
         <Stepper />
       </div>
-      <StepperControls/>
+      <StepperControls />
     </div>
   );
 }
