3b96aa36b15c52b56f8a47e16cfa479ad7aad209
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-10-16 12:45:16 +0530

    style: add stepper component styles

--- src/components/Stepper/Stepper.jsx
+++ src/components/Stepper/Stepper.jsx
@@ -1,3 +1,19 @@
 export const Stepper = () => {
-  return <div>This is a stepper.</div>;
+  const displaySteps = (
+    <div className="flex w-full items-center">
+      <div className="relative flex flex-col items-center text-teal-600">
+        <div className="rounded-full transition duration-500 ease-in-out border-2 border-gray-300 h-12 w-12 flex items-center justify-center py-3"></div>
+        <div className="absolute top-0 text-center mt-16 w-32 text-xs font-medium uppercase">
+          Description
+        </div>
+      </div>
+      {/* Line */}
+      <div className="flex-auto border-t-2 transition duration-500 ease-in-out"></div>
+    </div>
+  );
+  return (
+    <div className="mx-4 p-4 flex justify-between items-center">
+      {displaySteps}
+    </div>
+  );
 };
